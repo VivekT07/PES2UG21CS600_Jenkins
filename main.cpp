d2e4fa0404a59edae84a7a929c52c6e6c5657bcb
VivekT07 <92370508+VivekT07@users.noreply.github.com>
2024-03-10 18:15:49 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "webhook successfully enabled\n";
+  return 0;
+}
